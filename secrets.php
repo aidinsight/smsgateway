7e2f85e7e5c417afba47aa9cb54fda6680285385
Paul Stefaan Mooij <psmooij@gmail.com>
2018-09-26 23:11:15 +0200

    Create secrets.php

---
+++ secrets.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    Function file hidden from public  v 0.1
+    auteur PSMooij - 2019
+    licensed under apache 2.0
+*/
+$accountname = "xxxxx";
+$accounttoken = "xxxxxxx";
+ // TODO make it more nice to read but working getting around E.164+format
+$configsetup = "PhoneNumberSid=xxxxxx&MessagingServiceSid=xxxxxxx";
+?>
